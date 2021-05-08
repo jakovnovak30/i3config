cb3fba8b5d93880ea19e5228aee86c5cc9e62e43
jakovnovak30 <jakovnovak30@gmail.com>
2021-05-08 23:26:53 +0200

    bolja baterija

--- i3blocks/battery.sh
+++ i3blocks/battery.sh
@@ -2,9 +2,14 @@
 
 BAT=$(acpi -b | grep -E -o '[0-9]*%')
 
-# Full and short texts
-echo " $BAT"
-echo "BAT: $BAT"
+PUNI_SE=$(acpi | grep -o 'Charging')
+
+if [ -z $PUNI_SE ]
+	then
+		echo " $BAT"
+	else
+		echo " $BAT"
+fi
 
 # Set urgent flag below 5% or use orange below 20%
 [ ${BAT%?} -le 5 ] && exit 33
